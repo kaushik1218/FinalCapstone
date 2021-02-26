8dab0b73d93518abf0eadd0b0f20c676330b59ea
HarshPatel9462 <harshgpatel121@gmail.com>
2021-02-25 20:59:57 -0500

    added pagination and search feature with filter

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,85 +1,35 @@
-const Product = require('../models/product')
 
-// Create new  product => /api/v1/admin/product/new
-exports.newProduct = async(req, res, next) => {
+const APIFeatures = require('../utils/apiFeatures');
+
+
+//Create new product => /api/v1/admin/product/new
+exports.newProduct = catchAsyncErrors(async (req, res, next) => {    
+     
     const product = await Product.create(req.body);
 
     res.status(201).json({
-        success: true,
+        success:true,
         product
     })
-}
+})
+//Get all product
+exports.getProducts = catchAsyncErrors(async (req, res, next) => {
 
-// Get all products => /api/v1/products
+    const resPerPage = 4;
 
-exports.getProducts = async (req, res, next) => {
-    const products = await Product.find();
+    const productCount = await Product.countDocuments();// will count all the products on frontend
+
+    const apiFeatures = new APIFeatures(Product.find(), req.query)
+        .search()
+        .filter()
+        .pagination(resPerPage)
+    
+    const product = await apifeatures.query;
+
     res.status(200).json({
         success: true,
         count: products.length,
+        productCount,
         products
     })
-}
-
-// Get Single Product details => /api/v1/product/:id
-
-exports.getSingleProduct = async (req, res, next) => {
-    const product = await Product.findById(req.params.id);
-
-    if(!product) {
-        return res.status(404).json({
-            success: false,
-            message: 'Product not found'
-        })
-    }
-
-    res.status(200).json({
-        success: true,
-        product
-    })
-}
-
-//update Product => /api/v1/admin/product/:id
-exports.updateProduct = async (req, res, next) => {
-    let product = await Product.findById(req.params.id); 
-
-    if(!product) {
-        return res.status(404).json({
-            success: false,
-            message: 'Product not found'
-        })
-    }
-    
-    product = await Product.findByIdAndUpdate(req.params.id, req.body, {
-        new: true,
-        runValidators: true,
-        userFindAndModify: false
-    });
-
-    res.status(200).json({
-        success: true,
-        product
-    })
-    
-}
-
-// Delete Product => /api/v1/admin/product/:id
-
-exports.deleteProduct = async (req, res, next) => {
-
-    const product = await Product.findById(req.params.id);
-
-    if(!product) {
-        return res.status(404).json({
-            success: false,
-            message: 'Product not found'
-        })
-    }
-
-    await product.remove();
-
-    res.status(200).json({
-        success: true,
-        message: 'Product is deleted.'
-    })
-}
+})
